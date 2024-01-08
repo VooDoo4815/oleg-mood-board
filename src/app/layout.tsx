24776b4da98db020dd66d41a6d4129a1eb80c433
Voodoo <killer4815@mail.ru>
2024-01-09 00:04:15 +0300

    Убираем ненужный пакет

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import "./globals.css";
 import React from "react";
 
@@ -14,9 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-            </body>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }

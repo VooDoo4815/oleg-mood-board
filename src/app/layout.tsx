2afa90b8aa9d0618516e45f8d418d5882d9843fa
Voodoo <killer4815@mail.ru>
2024-01-21 16:28:12 +0300

    Добавляем мета-информацию для мессенеджеров

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,13 @@ export const metadata: Metadata = {
         description: "Какой ты Олег?",
         url: "https://voodoo4815.github.io/oleg-mood-board/",
         type: "website",
-        images: [{ url: "https://voodoo4815.github.io/oleg-mood-board/images/oleg-1.webp", height: 300, width: 300 }],
+        images: [
+            {
+                url: "https://voodoo4815.github.io/oleg-mood-board/images/78ccb62f0456e74b23905415c8456d0a.jpg",
+                height: 100,
+                width: 100,
+            },
+        ],
     },
 };
 
